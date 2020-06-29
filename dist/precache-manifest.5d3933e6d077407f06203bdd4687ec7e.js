968fbeb237facc311d746cc2408bb68dffb0e81f
lelb <lelb@users.noreply.github.com>
2020-06-29 12:40:31 +0300

    Built for heroku

---
+++ dist/precache-manifest.5d3933e6d077407f06203bdd4687ec7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f360dc967424b720976e",
+    "url": "/css/about.f44ab934.css"
+  },
+  {
+    "revision": "f360dc967424b720976e",
+    "url": "/js/about.6b2cdb17.js"
+  },
+  {
+    "revision": "cfe6e1553caee51db46e",
+    "url": "/css/app.bf42355a.css"
+  },
+  {
+    "revision": "cfe6e1553caee51db46e",
+    "url": "/js/app.193838bf.js"
+  },
+  {
+    "revision": "a29a6c1fddc7a09192ab",
+    "url": "/js/chunk-vendors.0935fc8f.js"
+  },
+  {
+    "revision": "6411bb6297adac928c0d5697b23ad078",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
